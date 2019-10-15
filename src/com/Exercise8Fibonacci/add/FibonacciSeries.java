313e590ff5fa2ac77e247ba9fc5b4eb6c4efd553
Alicia2714 <Alicia_Sanchez2714@hotmail.com>
2019-10-15 12:15:22 -0500

    Fibonacci exercise finished

--- src/com/Exercise8Fibonacci/add/FibonacciSeries.java
+++ src/com/Exercise8Fibonacci/add/FibonacciSeries.java
@@ -1,9 +1,37 @@
 package com.Exercise8Fibonacci.add;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Variable declaration
+		
+		long fibonacci = 1;
+		long contador1 = 0;
+		long contador2= 1;
+		int iteraciones =0;
+		
+		//Object declaration
+		Scanner input = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese la cantidad de iteraciones que desee: ");
+			iteraciones = input.nextInt();	
+			if(iteraciones<=0) {
+				System.out.println("Please, introduce a positive value.");
+			}
+		}while(iteraciones<=0);
+		
+		
+		for (int i=0; i<iteraciones; i++) {
+			System.out.println(fibonacci +", ");
+			
+			fibonacci = contador1+contador2;
+			contador1=contador2;
+			contador2=fibonacci;
+		}
+		
+		
 
 	}
 
